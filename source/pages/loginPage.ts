4070eb11741a61c2d6898480435f603dcbdd2dd6
ThanhPhong278 <phongvo2728@gmail.com>
2024-08-11 23:09:10 +0700

    fix_navigation_to_login_page_with_page_object_model_partten

--- source/pages/loginPage.ts
+++ source/pages/loginPage.ts
@@ -1,36 +1,33 @@
-
 import { expect, Page } from "@playwright/test";
 import PlaywrightWrapper from "../helper/wrapper/PlaywrightWrappers";
 
-
 export default class LoginPage {
+    private pageBase: PlaywrightWrapper;
 
-    private pageBase: PlaywrightWrapper
-    
     constructor(private page: Page) {
         this.pageBase = new PlaywrightWrapper(page);
     }
 
     private Elements = {
-        userInput: "'아이디 입력'",
-        passwordInput: "'비밀번호 입력'",
-        loginBtn: "'button', { name: '로그인' }'",
+        userInput: "아이디 입력",  
+        passwordInput: "비밀번호 입력", 
     }
 
     async navigateToLoginPage() {
         await this.pageBase.goto(process.env.BASEURL);
     }
-    async enterUserName(Username: string) {
-        await this.page.getByPlaceholder(this.Elements.userInput).fill(Username);
+
+    async enterUserName(username: string) {
+        await this.page.getByPlaceholder(this.Elements.userInput).fill(username);
     }
-    async enterPassword(Password: string) {
-        await this.page.getByPlaceholder(this.Elements.passwordInput).fill(Password);
+
+    async enterPassword(password: string) {
+        await this.page.getByPlaceholder(this.Elements.passwordInput).fill(password);
     }
 
     async clickLoginButton() {
-        await this.pageBase.waitAndClick(this.Elements.loginBtn);
-    }
-
+        await this.page.getByRole('button', { name: '로그인' }).click();
+}
     async loginUser(username: string, password: string) {
         await this.enterUserName(username);
         await this.enterPassword(password);
--- source/test/steps/loginSteps.ts
+++ source/test/steps/loginSteps.ts
@@ -9,47 +9,48 @@ const loginPage = new LoginPage(pageFixture.page);
 const assert = new Assert(pageFixture.page);
 setDefaultTimeout(60 * 1000 * 2);
 
-Given('User navigates to the application', async function () {
-    await pageFixture.page.goto(process.env.BASEURL);
-    pageFixture.logger.info("Navigated to the Salon Admin");
-});
-
-Given('User enter the password as {string}', async function (password) {
-    await pageFixture.page.getByPlaceholder('비밀번호 입력').fill(password);
-});
-
-Given('User enter the username as {string}', async function (username) {
-    await pageFixture.page.getByPlaceholder('아이디 입력').fill(username);
-});
-
-When('User click on the login button', async function () {
-    await pageFixture.page.getByRole('button', { name: '로그인' }).click();
-    await pageFixture.page.waitForLoadState();
-});
-
-Then('Login should be success', async function () {
-    const text = await pageFixture.page.getByRole('button', { name: 'vpsalon1kr' }).textContent();
-    console.log("Username: " + text);
-});
-
 // Given('User navigates to the application', async function () {
 //     await pageFixture.page.goto(process.env.BASEURL);
+//     pageFixture.logger.info("Navigated to the Salon Admin");
 // });
 
-// Given('User enter the password as', async function (password) {
-//     await loginPage.enterPassword(data.password);
+// Given('User enter the password as {string}', async function (password) {
+//     await pageFixture.page.getByPlaceholder('비밀번호 입력').fill(password);
 // });
 
-// Given('User enter the username as', async function (username) {
-//     await loginPage.enterUserName(data.username);
+// Given('User enter the username as {string}', async function (username) {
+//     await pageFixture.page.getByPlaceholder('아이디 입력').fill(username);
 // });
 
 // When('User click on the login button', async function () {
-//     await loginPage.clickLoginButton();
+//     await pageFixture.page.getByRole('button', { name: '로그인' }).click();
+//     await pageFixture.page.waitForLoadState();
 // });
 
 // Then('Login should be success', async function () {
 //     const text = await pageFixture.page.getByRole('button', { name: 'vpsalon1kr' }).textContent();
-//     assert.assertTextContains(text);
-
+//     console.log("Username: " + text);
 // });
+
+Given('User navigates to the application', async function () {
+    await loginPage.navigateToLoginPage();
+    pageFixture.logger.info("User navigates to the application to Ahasoft Login Page");
+});
+
+Given('User enter the username from test data', async function () {
+    await loginPage.enterUserName(data.username);
+});
+
+Given('User enter the password from test data', async function () {
+    await loginPage.enterPassword(data.password);
+});
+
+When('User click on the login button', async function () {
+    await loginPage.clickLoginButton();
+});
+
+Then('Login should be success', async function () {
+    const text = await pageFixture.page.getByRole('button', { name: 'vpsalon1kr' }).textContent();
+    assert.assertTextContains(text);
+
+});
