72275f9caf0709c0e172aaeff6c68c470c83bf81
ThanhPhong278 <phongvo2728@gmail.com>
2024-08-13 00:03:45 +0700

    fix_test_case_Login

--- source/test/steps/loginSteps.ts
+++ source/test/steps/loginSteps.ts
@@ -5,52 +5,31 @@ import LoginPage from "../../pages/loginPage";
 import Assert from "../../helper/wrapper/assert";
 import * as data from "../../helper/util/test-data/login.json"
 
-const loginPage = new LoginPage(pageFixture.page);
-const assert = new Assert(pageFixture.page);
 setDefaultTimeout(60 * 1000 * 2);
 
-// Given('User navigates to the application', async function () {
-//     await pageFixture.page.goto(process.env.BASEURL);
-//     pageFixture.logger.info("Navigated to the Salon Admin");
-// });
-
-// Given('User enter the password as {string}', async function (password) {
-//     await pageFixture.page.getByPlaceholder('비밀번호 입력').fill(password);
-// });
-
-// Given('User enter the username as {string}', async function (username) {
-//     await pageFixture.page.getByPlaceholder('아이디 입력').fill(username);
-// });
-
-// When('User click on the login button', async function () {
-//     await pageFixture.page.getByRole('button', { name: '로그인' }).click();
-//     await pageFixture.page.waitForLoadState();
-// });
-
-// Then('Login should be success', async function () {
-//     const text = await pageFixture.page.getByRole('button', { name: 'vpsalon1kr' }).textContent();
-//     console.log("Username: " + text);
-// });
-
 Given('User navigates to the application', async function () {
+    const loginPage = new LoginPage(pageFixture.page);
     await loginPage.navigateToLoginPage();
     pageFixture.logger.info("User navigates to the application to Ahasoft Login Page");
 });
 
 Given('User enter the username from test data', async function () {
+    const loginPage = new LoginPage(pageFixture.page);
     await loginPage.enterUserName(data.username);
 });
 
 Given('User enter the password from test data', async function () {
+    const loginPage = new LoginPage(pageFixture.page);
     await loginPage.enterPassword(data.password);
 });
 
 When('User click on the login button', async function () {
+    const loginPage = new LoginPage(pageFixture.page);
     await loginPage.clickLoginButton();
 });
 
 Then('Login should be success', async function () {
     const text = await pageFixture.page.getByRole('button', { name: 'vpsalon1kr' }).textContent();
+    const assert = new Assert(pageFixture.page);
     assert.assertTextContains(text);
-
 });
